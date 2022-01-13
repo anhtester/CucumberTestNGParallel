fde39d596bb4475a9cfbd011bcde871ed34a54fa
An Vo <vothaian97@gmail.com>
2022-01-14 00:17:25 +0700

    Update source

--- src/test/java/stepdefinitions/ProjectSteps.java
+++ src/test/java/stepdefinitions/ProjectSteps.java
@@ -1,17 +1,10 @@
 package stepdefinitions;
 
 import cucumber.TestContext;
-import io.cucumber.java.AfterStep;
-import io.cucumber.java.Scenario;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
-import utils.manager.DriverManager;
-import utils.manager.LocalDriverFactory;
 
 public class ProjectSteps extends BaseStepDef{
 
